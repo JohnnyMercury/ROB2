7eed4e8c7a5cf93973514d710507b50b8f571fe6
jakeLKM <j4cobm4dsen@gmail.com>
2026-05-10 11:41:54 +0200

    Test

--- Project/Open_map.m
+++ Project/Open_map.m
@@ -5,7 +5,7 @@ clc;
 close all;
 
 %% 1. Configuration
-inputFile = 'manual_custom_map.mat';
+inputFile = 'edited_custom_map.mat';
 
 %% 2. Load the Map Data
 fprintf('[LOAD] Loading %s...\n', inputFile);
